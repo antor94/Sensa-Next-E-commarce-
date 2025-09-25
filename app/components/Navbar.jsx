480ea43780c7dfcd04722e06ffd9a1b50283f138
Antor Web-Dev <antorremeni@gmail.com>
2025-09-25 17:38:52 +0600

    change

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    
+    
+    
+    
+    <nav>
+        <div className="container">
+            <div>
+                sfgsdfg
+            </div>
+        </div>
+    </nav>
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Navbar
