272c242712afce69d9893bf08dc45cfac5cff959
Antor Web-Dev <antorremeni@gmail.com>
2025-09-25 19:15:50 +0600

    chnage

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -17,8 +17,8 @@ const Banner = () => {
                     <div><p>Welcome to our online fashion haven, where we invite you to embark on a thrilling journey of discovery. We understand that fashion is an expression of your unique personality, and we've curated an extensive collection of dresses, shoes, and more to help you find your new favorites.</p></div>
 
                     <div>
-                        <div className='w-[113px] border border-[#6A6C68]'>   <Link href={'/'} ></Link>  </div>
-                        <div className='w-[113px] border border-[#6A6C68]'>   <Link href={'/'} ></Link>  </div>
+                        <div className='w-[113px] border border-[#6A6C68] hover:bg-[#8AC732]'>   <Link href={'/'} className='' >Shop Now</Link>  </div>
+                        <div className='w-[113px] border border-[#6A6C68]'>   <Link href={'/'} >Learn more</Link>  </div>
                     </div>
 
                 </div>
