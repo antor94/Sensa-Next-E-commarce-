bd154c51666ea2d78fedb8f9363efcf8f3a42da6
Antor Web-Dev <antorremeni@gmail.com>
2025-09-27 14:37:01 +0600

    chnage

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -8,7 +8,15 @@ const Footer = () => {
     
     <section id='footer'>
         <div className="container">
-            <div id=''>
+            <div id='footer-row'>
+
+                <div className='foo-col-1'>
+                    <h2>Make money with us</h2>
+                    <p>Sell product on Sansa</p>
+                    <p>Sell product on Sansa</p>
+                    <p>Sell product on Sansa</p>
+
+                </div>
 
             </div>
         </div>
