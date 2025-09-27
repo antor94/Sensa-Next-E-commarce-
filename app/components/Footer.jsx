338edb0d1e6a44a59eec2b23fcaa4d0a05782bc1
Antor Web-Dev <antorremeni@gmail.com>
2025-09-27 14:35:48 +0600

    chnage

---
+++ app/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    
+    
+    
+    <section id='footer'>
+        <div className="container">
+            <div id=''>
+
+            </div>
+        </div>
+    </section>
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Footer
